839dc0d89f42fe16cccc48cf45291a95dad71b0f
JHaralson25 <jacobharalson9@gmail.com>
2023-01-25 14:44:29 -0600

    Code Formatting

--- login.php
+++ login.php
@@ -1,46 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <link rel="stylesheet" href="login.css">
-        <title>Login</title>
-    </head>
-    <body>
-        <?php session_start();?>
-            <div class="login-container">
-                <h1 class="loginText">Login</h1>
-                    <div class="input-container">
-                        <form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="POST">
-                            <p>
-                                <label for="username">Username:</label>
-                                <input class="inputs" type="text" name="username" id="username">
-                            </p>
-                            </div>
-                            <div class="button-container">
-                                <input class="login-button" type="submit" value="Login">
-                            </div>
-                        </form>
-                    </div>
-            </div>
-        <?php
-            // Check that user input something
-            if (isset($_POST['username'])) {
-                $username = (string) $_POST['username'];
 
-                $h = fopen("/srv/module2/users.txt", "r");
+<head>
+    <link rel="stylesheet" href="login.css">
+    <title>Login</title>
+</head>
 
-                while(!feof($h)){
-                    if(trim(fgets($h)) == $username){
-                        // Update session variables
-                        @$_SESSION['status'] = true;
-                        @$_SESSION['username'] = $username;
+<body>
+    <?php session_start(); ?>
+    <div class="login-container">
+        <h1 class="loginText">Login</h1>
+        <div class="input-container">
+            <form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="POST">
+                <p>
+                    <label for="username">Username:</label>
+                    <input class="inputs" type="text" name="username" id="username">
+                </p>
+        </div>
+        <div class="button-container">
+            <input class="login-button" type="submit" value="Login">
+        </div>
+        </form>
+    </div>
+    </div>
+    <?php
+    // Check that user input something
+    if (isset($_POST['username'])) {
+        $username = (string) $_POST['username'];
 
-                        // Send to filesharing.php site
-                        fclose($h);
-                        header("Location: filesharing.php");
-                        exit;
-                    }
-                }
+        $h = fopen("/srv/module2/users.txt", "r");
+
+        while (!feof($h)) {
+            if (trim(fgets($h)) == $username) {
+                // Update session variables
+                @$_SESSION['status'] = true;
+                @$_SESSION['username'] = $username;
+
+                // Send to filesharing.php site
+                fclose($h);
+                header("Location: filesharing.php");
+                exit;
             }
-        ?>
-    </body>
+        }
+    }
+    ?>
+</body>
+
 </html>
